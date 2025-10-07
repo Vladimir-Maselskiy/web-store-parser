4caaedc86fbcb1e143efb15b99b87e4db8d03172
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-10-07 16:50:21 +0300

    chore: add hernandez extension id

--- src/utils/parseExtensions.ts
+++ src/utils/parseExtensions.ts
@@ -17,6 +17,7 @@ const paths = [
   'nbfdgjmapidikelppdieahmlddjinpjm', //logiline
   'nnfmkaglijgngnephnkgmaldmejandhk', //statvin
   'fojpkmgahmlajoheocnkebaoodepoekj', //autohelperbot
+  'oijclklaeblnkiokkgokednjnhkleffj', //hernandez
 ];
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '';
